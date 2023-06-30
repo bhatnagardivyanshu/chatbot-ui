0d5bd4135ecb760b3b197867b2071d909c6b692b
Divyanshu Bhatnagar <divyanshu.bhatnagar@adpushup.com>
2023-06-30 09:07:53 +0530

    Socket server URL update

--- utils/app/socket.ts
+++ utils/app/socket.ts
@@ -14,18 +14,17 @@ export interface SocketMessageReceived {
 
 export const initSocket = () => {
 	// TODO: specify io(server domain)
-	socket = io('http://localhost:4000', { transports : ['websocket'] });
+	// socket = io('http://localhost:8000', { transports : ['websocket'] });
+	socket = io('http://localhost:8000');
+
 	socket.io.on('open', () => {
-		console.log('client ready')
+		console.log('client ready');
 	})
-	socket.on('chat:resp', (response) => {
-		console.log('xxxxxxxxxxx', response)
-	});
 
 }
 
 export const sendMessage = (msg: string, chat_id: string) => {
-	console.log('sending to server', msg, chat_id)
+	// console.log('sending to server', msg, chat_id)
 	socket.emit("chat:msg", { msg, chat_id });
 }
 
